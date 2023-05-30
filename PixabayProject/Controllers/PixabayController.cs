f67f02a7acc6bf045015497b0645fccdf5168b2e
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2023-05-30 16:52:51 +0300

    A controller was created to search for paintings

---
+++ PixabayProject/Controllers/PixabayController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using PixabayProject.Models;
+using System;
+using System.Net.Http;
+
+namespace PixabayProject.Controllers
+{
+    public class PixabayController
+    {
+        public async Task<bool> FindPicture(Pixabay pixabay)
+        {
+            HttpClient httpClient = new HttpClient();
+            using var requestPass = new HttpRequestMessage(HttpMethod.Get, $"https://pixabay.com/api/?key=28501269-85beb46cbb93172460c9f97d2&q={Uri.EscapeDataString(pixabay.imageURL)}&image_type=photo");
+            using var responseSend = await httpClient.SendAsync(requestPass);
+            if (responseSend.IsSuccessStatusCode)
+            {
+                var responseContent = await responseSend.Content.ReadAsStringAsync();
+                JObject jsonResponse = JObject.Parse(responseContent);
+                JArray images = jsonResponse.Value<JArray>("hits");
+                return images.Count > 0;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
