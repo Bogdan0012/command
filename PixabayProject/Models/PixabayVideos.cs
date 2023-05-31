5c72a93b4b41681c2ee046451bc75a9168d33790
svt13 <svt13@ukr.net>
2023-06-01 01:02:47 +0300

    add user image for video

--- PixabayProject/Models/PixabayVideos.cs
+++ PixabayProject/Models/PixabayVideos.cs
@@ -5,6 +5,7 @@
         public int? views { get; set; }
         public int? likes { get; set; }
         public string? user { get; set; }
+        public string? userImageURL { get; set; }
         public VideoListModel? videos { get; set; }
     }
 }
